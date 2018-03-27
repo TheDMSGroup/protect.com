e42a0ff3d5b8301781027b1d2856eeda22031c0d
Nick Moncur <nmoncur@thedmsgrp.com>
2018-03-27 14:43:08 -0600

    Remove links from index.html that were 404'ing, add js/index.js, add js for switching out number based on utm source

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// find out if utm_source exists in array
+// replace the phone number in each instance on the page
+
+// default number for page
+var defaultNumber = '1-866-243-0621';
+
+// list of phone numbers to use for specific UTM sources
+// utm_source: 'phone number'
+var phoneNumbers = {
+    test: '888-888-8888'
+};
+
+$(function () {
+    // get utm_source param from url
+    let utmSource = getURLParameter('utm_source');
+
+    var number = defaultNumber;
+    // if the UTM source has its own number, set it as the number
+    if(typeof phoneNumbers[utmSource] !== 'undefined') {
+        var number = phoneNumbers[utmSource];
+    }
+
+    // update text fields based on number
+    $('.phone').text(number);
+    // update href's based on number
+    $('a.trackphone').attr('href', 'tel:'+number);
+});
+
+// some regex to get a url parameter
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
