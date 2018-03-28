b575df7f18f1d2d0f9a5b1d5dc64f11df956e5f1
Nick Moncur <nmoncur@thedmsgrp.com>
2018-03-28 09:26:02 -0600

    Update Javascript architecture in js/index.js to clean up document ready function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 // find out if utm_source exists in array
 // replace the phone number in each instance on the page
 
-// default number for page
+// default number for site
 var defaultNumber = '1-866-243-0621';
 
 // list of phone numbers to use for specific UTM sources
-// utm_source: 'phone number'
 var phoneNumbers = {
+    // utm_source: 'phone number'
     test: '888-888-8888'
 };
 
 $(function () {
+    getPhoneNumber();
+});
+
+function getPhoneNumber() {
     // get utm_source param from url
     let utmSource = getURLParameter('utm_source');
 
@@ -24,7 +28,7 @@ $(function () {
     $('.phone').text(number);
     // update href's based on number
     $('a.trackphone').attr('href', 'tel:'+number);
-});
+}
 
 // some regex to get a url parameter
 function getURLParameter(name) {
