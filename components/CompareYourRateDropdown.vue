0be2dbc021fd7d2e72b6265f5245075d90111a23
mvitello <mvitello@dmsgroup.com>
2025-11-18 15:09:23 -0500

    homepage and related components

--- components/CompareYourRateDropdown.vue
+++ components/CompareYourRateDropdown.vue
@@ -1,8 +1,32 @@
+<script setup>
+  const props = defineProps({
+    compareRateConfig: {
+      type: Object,
+      default: () => ({}),
+    },
+    submitText: {
+      type: String,
+      default: "Continue",
+    },
+  });
+
+  const { compareRateConfig, submitText } = props;
+
+  const goTo = ref("");
+  const router = useRouter();
+
+  const changeRoute = () => {
+    if (goTo.value !== "") {
+      router.push({ path: `/${goTo.value}/` });
+    }
+  };
+</script>
+
 <template>
   <div class="compare-your-rate-dropdown">
-    <selects v-model="goTo" :valid="true" :config="compareRateConfig"></selects>
-    <span class="click-continue" v-on:click="changeRoute">
-      <b-buttons
+    <SelectsMain v-model="goTo" :valid="true" :config="compareRateConfig" />
+    <span class="click-continue" @click="changeRoute">
+      <ButtonsMain
         class="continue"
         :config="{
           size: 'lg',
@@ -10,110 +34,79 @@
           label: submitText,
           icon: 'arrow-right-short',
         }"
-      ></b-buttons>
+      />
     </span>
   </div>
 </template>
 
-<script>
-export default {
-  name: "CompareYourRateDropdown",
-  props: {
-    compareRateConfig: Object,
-    submitText: String,
-  },
-  data() {
-    return {
-      goTo: "",
-      form: {
-        vertical: "test",
-      },
-    };
-  },
-  methods: {
-    changeRoute() {
-      if (this.goTo !== "") {
-        this.$router.push({ path: `/${this.goTo}/` });
-      }
-    },
-    required(field) {
-      return this.form[field.split(".")[1]].length > 0;
-    },
-    wat(field, value) {
-      this.form[field.split(".")[1]] = value;
-    },
-  },
-};
-</script>
-
 <style lang="scss">
-// TO DO: edit styles for this component
-.compare-your-rate-dropdown {
-  display: flex;
+  // TO DO: edit styles for this component
+  .compare-your-rate-dropdown {
+    display: flex;
 
-  @include media-breakpoint-down(lg) {
-    flex-wrap: wrap;
-    position: relative;
+    @include media-breakpoint-down(lg) {
+      flex-wrap: wrap;
+      position: relative;
+    }
+
+    .selects-component {
+      position: static;
+    }
+
+    .valid {
+      z-index: 100;
+
+      @include media-breakpoint-down(sm) {
+        width: 100%;
+      }
+
+      .list-group {
+        background-color: #fff;
+        position: absolute;
+        width: auto;
+        min-width: 500px;
+
+        @include media-breakpoint-down(xl) {
+          min-width: 300px;
+        }
+        @include media-breakpoint-down(lg) {
+          position: absolute;
+          width: 100%;
+        }
+      }
+
+      .input-group {
+        background-color: $white;
+
+        input {
+          color: $gray-dark;
+        }
+      }
+    }
   }
 
-  .selects-component {
-    position: static;
-  }
+  .click-continue {
+    margin: 0 1em;
+    display: block;
 
-  .valid {
-    z-index: 100;
-
-    @include media-breakpoint-down(sm) {
+    @include media-breakpoint-down(lg) {
+      margin: 1em 0;
       width: 100%;
     }
 
-    .list-group {
-      background-color: #fff;
-      position: absolute;
-      width: auto;
-      min-width: 500px;
+    .continue {
+      button {
+        display: flex;
+        height: 80px;
+        padding: 1em 1.2em;
+        align-items: center;
+        font-weight: 500;
 
-      @include media-breakpoint-down(xl) {
-        min-width: 300px;
-      }
-      @include media-breakpoint-down(lg) {
-        position: absolute;
-        width: 100%;
-      }
-    }
-
-    .input-group {
-      background-color: $white;
-
-      input {
-        color: $gray-dark;
+        @include media-breakpoint-down(lg) {
+          width: 100%;
+          justify-content: center;
+        }
       }
     }
   }
-}
-
-.click-continue {
-  margin: 0 1em;
-  display: block;
-
-  @include media-breakpoint-down(lg) {
-    margin: 1em 0;
-    width: 100%;
-  }
-
-  .continue {
-    button {
-      display: flex;
-      height: 80px;
-      padding: 1em 1.2em;
-      align-items: center;
-      font-weight: 500;
-
-      @include media-breakpoint-down(lg) {
-        width: 100%;
-        justify-content: center;
-      }
-    }
-  }
-}
 </style>
--- components/VerticalHero.vue
+++ components/VerticalHero.vue
@@ -1,3 +1,40 @@
+<script setup>
+  import { buildImageUrl } from "@/composables/images";
+  const props = defineProps({
+    heroImage: {
+      type: String,
+      default: "",
+    },
+    headline: {
+      type: String,
+      default: "",
+    },
+    subheadline: {
+      type: String,
+      default: "",
+    },
+    ctaType: {
+      type: String,
+      default: "rates-dropdown",
+    },
+    ctaConfig: {
+      type: Object,
+      default: () => ({}),
+    },
+    heroClass: {
+      type: String,
+      default: "",
+    },
+    subheadlineType: {
+      type: String,
+      default: "large",
+    },
+  });
+
+  const { heroImage, headline, subheadline, ctaType, ctaConfig, heroClass, subheadlineType } = props;
+  console.log(ctaConfig, ctaType);
+</script>
+
 <template>
   <div class="hero-wrapper vertical-hero">
     <b-container :class="heroClass">
@@ -10,262 +47,219 @@
               <p v-else>{{ subheadline }}</p>
             </b-col>
             <b-col v-if="ctaType" class="cta-col" cols="12">
-              <b-row v-if="ctaType == 'buttons'" class="btn-row">
-                <b-col
-                  v-for="(btn, index) in ctaConfig.options"
-                  :key="index"
-                  cols="12"
-                  lg="6"
-                >
-                  <router-link class="vertical-btn" :to="'/' + btn.value">
-                    <img :src="getImage(btn.icon)" :alt="btn.text" />
+              <b-row v-if="ctaType && ctaType == 'buttons'" class="btn-row">
+                <b-col v-for="(btn, index) in ctaConfig.options" :key="index" cols="12" lg="6">
+                  <NuxtLink class="vertical-btn" :to="'/' + btn.value">
+                    <NuxtImg :src="buildImageUrl(btn.icon)" :alt="btn.text" />
                     <p class="btn-title">
                       {{ btn.text }}
                     </p>
-                  </router-link>
+                  </NuxtLink>
                 </b-col>
               </b-row>
-              <div
-                v-else-if="ctaType == 'rates-dropdown'"
-                class="compare-dropdown"
-              >
-                <CompareYourRateDropdown
-                  submitText="CONTINUE"
-                  :compareRateConfig="ctaConfig"
-                />
+              <div v-else-if="ctaType && ctaType == 'rates-dropdown'" class="compare-dropdown">
+                <CompareYourRateDropdown submit-text="CONTINUE" :compare-rate-config="ctaConfig" />
               </div>
             </b-col>
           </b-row>
         </div>
         <div class="hero-right">
-          <img :src="getImage(heroImage)" alt="Identity Shield" />
+          <NuxtImg :src="buildImageUrl(heroImage)" alt="Identity Shield" />
         </div>
       </div>
     </b-container>
-    <div class="skews"></div>
+    <div class="skews" />
   </div>
 </template>
 
-<script>
-export default {
-  name: "VerticalHero",
-  props: {
-    heroImage: String,
-    headline: String,
-    subheadline: String,
-    ctaType: String,
-    ctaConfig: Object,
-    heroClass: {
-      type: String,
-      default: "",
-    },
-    subheadlineType: {
-      type: String,
-      default: "large",
-    },
-  },
-  components: {
-    BContainer,
-    BRow,
-    BCol,
-    CompareYourRateDropdown,
-  },
-  methods: {
-    getImage(image) {
-      return "../public/assets/" + image;
-    },
-  },
-};
-</script>
-
 <style scoped lang="scss">
-.hero-wrapper {
-  background-color: $gray-light;
-  width: 100%;
-  overflow: hidden;
+  .hero-wrapper {
+    background-color: $gray-light;
+    width: 100%;
+    overflow: hidden;
 
-  .container {
-    z-index: -1;
-  }
+    .container {
+      z-index: -1;
+    }
 
-  .container.home-seo-hero {
+    .container.home-seo-hero {
+      .hero {
+        .hero-left {
+          width: 50%;
+          @include media-breakpoint-down(md) {
+            width: 100%;
+          }
+          h1 {
+            font-size: 3.75rem;
+            @include media-breakpoint-down(xl) {
+              font-size: 3rem;
+            }
+          }
+          p {
+            @include media-breakpoint-down(xl) {
+              font-size: 1rem;
+              line-height: 1.4;
+              max-width: 500px;
+            }
+          }
+        }
+      }
+    }
+
     .hero {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      height: 700px;
+      flex-wrap: wrap;
+
+      @include media-breakpoint-down(md) {
+        height: auto;
+      }
+
       .hero-left {
-        width: 50%;
+        width: 55%;
+        display: flex;
+        flex-direction: column;
+        margin-top: 6em;
+        text-align: left;
+
+        @include media-breakpoint-down(lg) {
+          margin-top: 4em;
+        }
+
         @include media-breakpoint-down(md) {
           width: 100%;
+          margin-bottom: 2em;
+        }
+
+        @include media-breakpoint-down(sm) {
+          margin-top: 3em;
+          margin-bottom: 0;
         }
         h1 {
-          font-size: 3.75rem;
+          font-size: 4.3rem;
+          font-family: "Cantata One", serif;
+          letter-spacing: -0.07rem;
+          line-height: 4.9rem;
+
           @include media-breakpoint-down(xl) {
-            font-size: 3rem;
+            font-size: 3.3em;
+            line-height: 1.2;
+          }
+          @include media-breakpoint-down(lg) {
+            font-size: 2.5em;
+            max-width: 450px;
           }
         }
-        p {
-          @include media-breakpoint-down(xl) {
-            font-size: 1rem;
-            line-height: 1.4;
-            max-width: 500px;
+
+        h2 {
+          color: $gray-dark;
+          font-size: 2.39rem;
+          font-family: "Nunito Sans", sans-serif;
+          font-weight: 400;
+          margin-top: 1.3rem;
+          margin-bottom: 2.3rem;
+          letter-spacing: -0.019rem;
+          line-height: 2.85rem;
+
+          @include media-breakpoint-down(lg) {
+            font-size: 1.6em;
+            line-height: 1.3em;
+          }
+        }
+
+        .mobile-shield-image {
+          width: 100%;
+          max-height: 400px;
+          object-fit: contain;
+          object-position: right;
+        }
+        .cta-col {
+          position: static;
+
+          @include media-breakpoint-down(xs) {
+            padding-bottom: 50px;
           }
         }
       }
-    }
-  }
 
-  .hero {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    height: 700px;
-    flex-wrap: wrap;
-
-    @include media-breakpoint-down(md) {
-      height: auto;
-    }
-
-    .hero-left {
-      width: 55%;
-      display: flex;
-      flex-direction: column;
-      margin-top: 6em;
-      text-align: left;
-
-      @include media-breakpoint-down(lg) {
-        margin-top: 4em;
-      }
-
-      @include media-breakpoint-down(md) {
-        width: 100%;
-        margin-bottom: 2em;
-      }
-
-      @include media-breakpoint-down(sm) {
-        margin-top: 3em;
-        margin-bottom: 0;
-      }
-      h1 {
-        font-size: 4.3rem;
-        font-family: "Cantata One", serif;
-        letter-spacing: -0.07rem;
-        line-height: 4.9rem;
-
-        @include media-breakpoint-down(xl) {
-          font-size: 3.3em;
-          line-height: 1.2;
-        }
-        @include media-breakpoint-down(lg) {
-          font-size: 2.5em;
-          max-width: 450px;
-        }
-      }
-
-      h2 {
-        color: $gray-dark;
-        font-size: 2.39rem;
-        font-family: "Nunito Sans", sans-serif;
-        font-weight: 400;
-        margin-top: 1.3rem;
-        margin-bottom: 2.3rem;
-        letter-spacing: -0.019rem;
-        line-height: 2.85rem;
-
-        @include media-breakpoint-down(lg) {
-          font-size: 1.6em;
-          line-height: 1.3em;
-        }
-      }
-
-      .mobile-shield-image {
-        width: 100%;
-        max-height: 400px;
-        object-fit: contain;
-        object-position: right;
-      }
-      .cta-col {
-        position: static;
-
-        @include media-breakpoint-down(xs) {
-          padding-bottom: 50px;
-        }
-      }
-    }
-
-    .hero-right {
-      width: 45%;
-      display: flex;
-      height: 100%;
-
-      @include media-breakpoint-down(md) {
-        width: 100%;
-      }
-
-      img {
-        display: block;
+      .hero-right {
+        width: 45%;
+        display: flex;
         height: 100%;
-        object-fit: contain;
-        object-position: top right;
 
         @include media-breakpoint-down(md) {
-          max-width: 600px;
-          margin: 0 auto;
           width: 100%;
+        }
+
+        img {
+          display: block;
+          height: 100%;
+          object-fit: contain;
+          object-position: top right;
+
+          @include media-breakpoint-down(md) {
+            max-width: 600px;
+            margin: 0 auto;
+            width: 100%;
+          }
+        }
+      }
+
+      .btn-row {
+        max-width: 600px;
+      }
+
+      .vertical-btn {
+        display: flex;
+        background: #fff;
+        border: 1px solid #ddd;
+        box-shadow: 0px 0px 16px -5px rgba(0, 0, 0, 0.1);
+        border-radius: 5px;
+        padding: 15px;
+        align-items: center;
+
+        &:hover {
+          text-decoration: none;
+        }
+
+        @include media-breakpoint-down(md) {
+          margin-bottom: 15px;
+        }
+
+        img {
+          height: 50px;
+          width: 50px;
+          object-fit: contain;
+          margin-right: 15px;
+        }
+        .btn-title {
+          color: #3b54ba;
+          font-size: 18px;
+          margin: 0;
+          font-weight: 400;
         }
       }
     }
+  }
 
-    .btn-row {
-      max-width: 600px;
-    }
+  .skews {
+    position: relative;
 
-    .vertical-btn {
-      display: flex;
-      background: #fff;
-      border: 1px solid #ddd;
-      box-shadow: 0px 0px 16px -5px rgba(0, 0, 0, 0.1);
-      border-radius: 5px;
-      padding: 15px;
-      align-items: center;
+    &:after {
+      content: "";
+      background-color: $white;
+      position: absolute;
+      height: 250px;
+      z-index: 0;
+      bottom: -175px;
+      width: 100%;
+      clip-path: ellipse(55% 125px at 50% 50%);
 
-      &:hover {
-        text-decoration: none;
-      }
-
-      @include media-breakpoint-down(md) {
-        margin-bottom: 15px;
-      }
-
-      img {
-        height: 50px;
-        width: 50px;
-        object-fit: contain;
-        margin-right: 15px;
-      }
-      .btn-title {
-        color: #3b54ba;
-        font-size: 18px;
-        margin: 0;
-        font-weight: 400;
+      @include media-breakpoint-down(sm) {
+        clip-path: ellipse(70% 95px at 50% 50%);
       }
     }
   }
-}
-
-.skews {
-  position: relative;
-
-  &:after {
-    content: "";
-    background-color: $white;
-    position: absolute;
-    height: 250px;
-    z-index: 0;
-    bottom: -175px;
-    width: 100%;
-    clip-path: ellipse(55% 125px at 50% 50%);
-
-    @include media-breakpoint-down(sm) {
-      clip-path: ellipse(70% 95px at 50% 50%);
-    }
-  }
-}
 </style>
