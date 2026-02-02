c2e8fafc31aceb742d0c011635ed5a19ad977b9d
matt vitello <50754095+vit-the-jedi@users.noreply.github.com>
2026-02-02 13:50:54 -0500

    Feature/city page breadcrumbs (#64)
    * updated api to return other cities in state, styling changes, and sitemap updates
    
    * mobile layout updates
    
    ---------
    
    Co-authored-by: mvitello <mvitello@dmsgroup.com>

--- pages/car-insurance/[state]/[city].vue
+++ pages/car-insurance/[state]/[city].vue
@@ -29,6 +29,15 @@
       <div class="container">
         <div class="hero-content">
           <div class="hero-text">
+            <nav aria-label="Breadcrumb" class="breadcrumb-nav pb-4 m-auto">
+              <NuxtLink to="/car-insurance">Car Insurance</NuxtLink>
+              <span> / </span>
+              <NuxtLink :to="`/car-insurance/${stateNameSlug}`">{{
+                stateName
+              }}</NuxtLink>
+              <span> / </span>
+              <span>{{ cityName }}</span>
+            </nav>
             <h1>{{ cityName }} Car Insurance</h1>
             <p class="subtitle">
               Compare quotes from top providers in {{ cityName }},
@@ -365,6 +374,23 @@
       </div>
     </section>
 
+    <section class="other-cities">
+      <div class="container">
+        <div class="cities-grid">
+          <div v-for="city in otherCities" :key="city" class="city-card">
+            <NuxtLink
+              :to="`/car-insurance/${stateNameSlug}/${city
+                .toLowerCase()
+                .replace(/ /g, '-')}`"
+              class="city-link"
+            >
+              {{ city }} Car Insurance
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <!-- Methodology -->
     <section class="methodology">
       <div class="container">
@@ -420,7 +446,10 @@
     stateFaultType,
     faq,
     rateComparison,
+    otherCities,
   } = await useCityDataFromCacheOrApi();
+
+  console.log("otherCities:", otherCities);
 
   async function useCityDataFromCacheOrApi() {
     const cacheKey = `${stateNameSlug}-${cityNameSlug}-car-insurance-data`;
@@ -574,6 +603,9 @@
     const zipCodeUrl = computed(() => {
       return "/get-quote";
     });
+    const otherCities = computed(() => {
+      return cityInfo.value ? cityInfo.value.otherCities : [];
+    });
 
     return {
       error,
@@ -598,6 +630,7 @@
       faq,
       zipCodeUrl,
       rateComparison,
+      otherCities,
     };
   }
 
@@ -751,6 +784,15 @@
     }
 
     .hero-text {
+      .breadcrumb-nav {
+        a {
+          text-decoration: underline;
+          color: $gray-dark;
+        }
+        span {
+          color: $gray-dark;
+        }
+      }
       h1 {
         font-size: 52px;
         font-weight: 700;
@@ -780,7 +822,7 @@
         gap: 12px;
         margin-bottom: 32px;
 
-        @include mobile {
+        @include tablet {
           max-width: 75%;
           margin: 0 auto 30px auto;
         }
@@ -820,6 +862,7 @@
       gap: 20px;
       width: 100%;
       max-width: 600px;
+      margin: auto;
       height: 90%;
 
       @include mobile {
@@ -899,6 +942,10 @@
       box-shadow: 0 4px 12px rgba(59, 130, 246, 0.2);
       max-width: 480px;
       width: 100%;
+
+      @include tablet {
+        margin: 32px auto 0 auto;
+      }
 
       @include mobile {
         margin-top: 32px;
@@ -1285,6 +1332,16 @@
     grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
     gap: 40px;
     margin-bottom: 48px;
+
+    @include tablet {
+      grid-template-columns: repeat(2, 1fr);
+
+      .step:nth-child(3) {
+        grid-column: 1 / -1;
+        max-width: 50%;
+        margin: 0 auto;
+      }
+    }
   }
 
   .step {
@@ -1362,6 +1419,14 @@
     a {
       font-size: 14px;
       text-decoration: underline;
+    }
+  }
+
+  .other-cities {
+    .city-link {
+      font-size: 0.95rem;
+      text-decoration: underline;
+      color: $blue-light;
     }
   }
 </style>
