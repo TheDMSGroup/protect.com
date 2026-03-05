7783ae26b75beeb70a219dfda6eeb9db12cf67d0
Gian Aguirre <>
2026-03-05 15:58:39 -0800

    capitalize names, map y, n to correct values, trusted form, url params mapped to right places

--- middleware/referrer-detection.global.js
+++ middleware/referrer-detection.global.js
@@ -1,56 +1,89 @@
 import { useStore } from "~/stores/store";
 import { getUeidByReferrer, REFERRER_MST } from "~/utils/referrer-detection";
 
+// All tracking params we want to persist
+const TRACKING_PARAMS = [
+  'gclid', 'msclkid', 'fbc', 'fbp', 'fbclid', 'clickid', 'rtclid',
+  'campaignid', 'ueid', 'variant', 'referrer', 'adgroupid', 'accountid',
+  'targetid', 'gbraid', 'wbraid', 'segment',
+  'utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'
+];
+
 export default defineNuxtRouteMiddleware((to) => {
   const store = useStore();
   const visitorInfoFromStore = store.visitorInfo;
 
-  //exit early if we already have ueid and mst
-  if (visitorInfoFromStore.ueid && visitorInfoFromStore.mst) {
-    return;
-  }
-  const ueidCookie = useCookie("ueid", { maxAge: 60 * 60 }); // 1 hour
+  // Create cookies for all tracking params
+  const cookies = {};
+  TRACKING_PARAMS.forEach(param => {
+    cookies[param] = useCookie(param, { maxAge: 60 * 60 }); // 1 hour
+  });
   const mstCookie = useCookie("mst", { maxAge: 60 * 60 });
 
-  const ueidParam = to.query.ueid;
-  const mstParam = to.query.mst;
+  // Facebook sets these cookies automatically
+  const fbcCookie = useCookie("_fbc");
+  const fbpCookie = useCookie("_fbp");
 
-  if (ueidParam || mstParam) {
-    const visitorInfo = {};
-    if (ueidParam) {
-      ueidCookie.value = ueidParam;
-      visitorInfo.ueid = ueidParam;
+  // Check if we have any new params in the URL
+  const visitorInfo = {};
+  let hasNewParams = false;
+
+  TRACKING_PARAMS.forEach(param => {
+    const paramValue = to.query[param];
+    if (paramValue) {
+      cookies[param].value = paramValue;
+      visitorInfo[param] = paramValue;
+      hasNewParams = true;
+    } else if (cookies[param].value && !visitorInfoFromStore[param]) {
+      // Restore from cookie if not in store
+      visitorInfo[param] = cookies[param].value;
+      hasNewParams = true;
     }
-    if (mstParam) {
-      mstCookie.value = mstParam;
-      visitorInfo.mst = mstParam;
-    }
+  });
+
+  // Fallback to Facebook's _fbc and _fbp cookies if not set
+  if (!visitorInfo.fbc && !visitorInfoFromStore.fbc && fbcCookie.value) {
+    visitorInfo.fbc = fbcCookie.value;
+    hasNewParams = true;
+  }
+  if (!visitorInfo.fbp && !visitorInfoFromStore.fbp && fbpCookie.value) {
+    visitorInfo.fbp = fbpCookie.value;
+    hasNewParams = true;
+  }
+
+  // Handle mst param
+  const mstParam = to.query.mst;
+  if (mstParam) {
+    mstCookie.value = mstParam;
+    visitorInfo.mst = mstParam;
+    hasNewParams = true;
+  } else if (mstCookie.value && !visitorInfoFromStore.mst) {
+    visitorInfo.mst = mstCookie.value;
+    hasNewParams = true;
+  }
+
+  if (hasNewParams) {
     store.setVisitorInfo(visitorInfo);
     return;
   }
 
-  if (ueidCookie.value) {
-    store.setVisitorInfo({
-      ueid: ueidCookie.value,
-      mst: mstCookie.value || null,
-    });
-    return;
-  }
+  // Fallback: detect referrer if no ueid
+  if (!visitorInfoFromStore.ueid && !cookies.ueid.value) {
+    const referrerParam = to.query.referrer;
+    let referrer = referrerParam;
 
-  const referrerParam = to.query.referrer;
-  let referrer = referrerParam;
+    if (!referrer) {
+      const headers = useRequestHeaders(["referer"]);
+      referrer = headers.referer;
+    }
 
-  if (!referrer) {
-    const headers = useRequestHeaders(["referer"]);
-    referrer = headers.referer;
-  }
-
-  if (referrer) {
-    const ueid = getUeidByReferrer(referrer);
-    if (ueid) {
-      ueidCookie.value = ueid;
-      mstCookie.value = REFERRER_MST;
-      store.setVisitorInfo({ ueid, mst: REFERRER_MST });
+    if (referrer) {
+      const ueid = getUeidByReferrer(referrer);
+      if (ueid) {
+        cookies.ueid.value = ueid;
+        mstCookie.value = REFERRER_MST;
+        store.setVisitorInfo({ ueid, mst: REFERRER_MST });
+      }
     }
   }
 });
